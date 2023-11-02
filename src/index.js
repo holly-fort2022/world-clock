fff7b1a178e2f2ac649a48bedd2dd11e116cce67
holly-fort2022 <holly.fort2022@gmail.com>
2023-11-02 16:05:46 -0400

    Added third city to the main page

--- src/index.js
+++ src/index.js
@@ -20,6 +20,16 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  let berlinElement = document.querySelector("#berlin");
+  if (berlinElement) {
+    let berlinDateElement = berlinElement.querySelector(".date");
+    let berlinTimeElement = berlinElement.querySelector(".time");
+    let berlinTime = moment().tz("Europe/Berlin");
+    berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+    berlinTimeElement.innerHTML = berlinTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
